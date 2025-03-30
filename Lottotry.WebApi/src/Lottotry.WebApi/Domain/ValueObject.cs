4fa86a74b401a55ad29b0d35c6f8870b20a05d14
hma14 <henryma14@gmail.com>
2025-03-29 21:32:03 -0700

    Added new table Users

---
+++ Lottotry.WebApi/src/Lottotry.WebApi/Domain/ValueObject.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Lottotry.WebApi.Domain
+{
+    public class ValueObject
+    {
+        public virtual Guid Id { get; set; }
+    }
+}
---
+++ Lottotry.WebApi/src/Lottotry.WebApi/Migrations/20250329200812_Add_Users_Table.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lottotry.WebApi.Migrations
+{
+    public partial class Add_Users_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Email",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Email", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmailId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Email_EmailId",
+                        column: x => x.EmailId,
+                        principalTable: "Email",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_EmailId",
+                table: "Users",
+                column: "EmailId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Email");
+
+        }
+    }
+}
